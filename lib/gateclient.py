3be3ea0781d1b0a82403424a279434e77942c927
Olga Zdanchuk <oz@tpro.lv>
2014-02-24 15:20:59 +0200

    creation python project first steps - tests & lib folders

---
+++ lib/gateclient.py
@@ -0,0 +1,8 @@
+__author__ = 'olga'
+
+
+class GateClient:
+
+   def __init__(self, accessData, requestExecuter):
+       self.accessData = accessData
+       self.requestExecutor = requestExecuter
---
+++ tests/test_gateclient.py
@@ -0,0 +1,6 @@
+__author__ = 'olga'
+
+from lib.gateclient import GateClient
+
+def test_number_3():
+    assert 3 == 3
