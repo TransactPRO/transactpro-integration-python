2df5a7b0baf16ce91b2171865e58afa72551078e
Olga Zdanchuk <zdanchuk@gmail.com>
2014-03-01 16:19:14 +0200

    Added mocked client tests for init and charge

--- tests/test_gateclient.py
+++ tests/test_gateclient.py
@@ -3,8 +3,9 @@ __author__ = 'olga'
 import hashlib
 from unittest import TestCase
 from lib.gateclient import GateClient
+from mock import patch, Mock
 
-class testGateClient(TestCase):
+class TestGateClient(TestCase):
 
     init_data = {
             'rs': 'AAAA',
@@ -44,3 +45,38 @@ class testGateClient(TestCase):
 
     def test_constructor_check_verifySSL_success(self):
         self.assertTrue(self.gate_client.access_data['verifySSL'])
+
+    @patch('lib.gateclient.Validator')
+    @patch('lib.gateclient.Request')
+    def test_init_request(self, req_mock, validator_mock):
+        validator_class = Mock()
+        validator_class.execute.return_value = {}
+        validator_mock.return_value = validator_class
+
+        req_class = Mock()
+        req_class.executeRequest.return_value = None
+        req_mock.return_value = req_class
+
+        gate_client = GateClient('https://www.payment-api.com', 'AAAA-AAAA-AAAA-AAAA', '111')
+
+        initial_data = {}
+        result_data = gate_client.init(initial_data)
+        expected_data = { 'guid': 'AAAA-AAAA-AAAA-AAAA', 'account_guid': 'AAAA-AAAA-AAAA-AAAA'}
+        expected_data['pwd'] = hashlib.sha1('111').hexdigest()
+        req_class.executeRequest.assert_called_once_with('init', expected_data)
+
+    @patch('lib.gateclient.Request')
+    def test_charge_request(self, req_mock):
+        req_class = Mock()
+        req_class.executeRequest.return_value = None
+        req_mock.return_value = req_class
+
+        gate_client = GateClient('https://www.payment-api.com', 'AAAA-AAAA-AAAA-AAAA', '111')
+
+        initial_data = {'init_transaction_id': 1, 'cc': '1234123412341234', 'cvv': '666', 'expire': '12/13', 'f_extended': 5}
+        result_data = gate_client.charge(initial_data)
+        expected_data = initial_data
+        expected_data['guid'] = 'AAAA-AAAA-AAAA-AAAA'
+        expected_data['account_guid'] = 'AAAA-AAAA-AAAA-AAAA'
+        expected_data['pwd'] = hashlib.sha1('111').hexdigest()
+        req_class.executeRequest.assert_called_once_with('charge', expected_data)
