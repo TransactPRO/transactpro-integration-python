ad7630ee2cb47ad16ba966e9016c08ad2dc4771b
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2016-03-09 16:21:50 +0300

    incepted setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+        name='transact-pro-integration-python',
+        description='TransactPRO Python integration',
+        long_description=read('README.md'),
+        author='Olga Zdanchuk',
+        author_email='zdanchuk@gmail.com',
+        packages=['transact-pro-integration-python'],
+        version='0.1',
+        url='https://github.com/TransactPRO/transactpro-integration-python',
+        keywords=[],
+        license='BSD',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Topic :: Internet :: WWW/HTTP :: WSGI',
+            'Framework :: Django',
+        ],
+)
